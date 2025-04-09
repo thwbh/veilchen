d5636a95b9a37497242c4b7d4499430d7715a686
Stefan Poindl <stefan@tohuwabohu.io>
2025-04-09 19:21:34 +0200

    cleanup index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,10 +6,9 @@ import RangeInput from './input/RangeInput.svelte';
 
 import ButtonGroup from './input/ButtonGroup.svelte';
 
-import Step from './stepper/Step.svelte';
 import Stepper from './stepper/Stepper.svelte';
 
 import Stack from './stack/Stack.svelte';
 
 export { KeyValuePair };
-export { RangeInput, ButtonGroup, Stepper, Step, Stack };
+export { RangeInput, ButtonGroup, Stepper, Stack };
