49eec6a7d61a05d962e7ee63a5f7068e3aef46db
Stefan Poindl <stefan@tohuwabohu.io>
2025-04-14 22:36:29 +0200

    add missing ValidatedInput export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,22 +2,19 @@
 import type { KeyValuePair, ListPickerData, ListPickerLabel } from './types/types.js';
 import type { ChartProps } from './chart/props.js';
 
-/** components go here */
-import AlertBox from '$lib/components/alert/AlertBox.svelte';
-
-import ListPicker from '$lib/components/list/ListPicker.svelte';
-
-import RangeInput from '$lib/components/input/RangeInput.svelte';
-
-import ButtonGroup from '$lib/components/input/ButtonGroup.svelte';
-
-import Stepper from '$lib/components/stepper/Stepper.svelte';
-
-import Stack from '$lib/components/stack/Stack.svelte';
-import { AlertType } from './enum/enum.js';
-
 export type { KeyValuePair, ListPickerData, ListPickerLabel };
 export type { ChartProps };
 
+/** components go here */
+import AlertBox from '$lib/components/alert/AlertBox.svelte';
+import ListPicker from '$lib/components/list/ListPicker.svelte';
+import RangeInput from '$lib/components/input/RangeInput.svelte';
+import ButtonGroup from '$lib/components/input/ButtonGroup.svelte';
+import Stepper from '$lib/components/stepper/Stepper.svelte';
+import Stack from '$lib/components/stack/Stack.svelte';
+import ValidatedInput from '$lib/components/input/ValidatedInput.svelte';
+
+import { AlertType } from './enum/enum.js';
+
 export { AlertType };
-export { AlertBox, ListPicker, RangeInput, ButtonGroup, Stepper, Stack };
+export { AlertBox, ListPicker, RangeInput, ButtonGroup, Stepper, Stack, ValidatedInput };
