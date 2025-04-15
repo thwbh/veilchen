926f45a480c2e824be96159673597a7b7c27d948
Stefan Poindl <stefan@tohuwabohu.io>
2025-04-15 22:34:21 +0200

    add ModalDialog export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -13,8 +13,9 @@ import ButtonGroup from '$lib/components/input/ButtonGroup.svelte';
 import Stepper from '$lib/components/stepper/Stepper.svelte';
 import Stack from '$lib/components/stack/Stack.svelte';
 import ValidatedInput from '$lib/components/input/ValidatedInput.svelte';
+import ModalDialog from '$lib/components/modal/ModalDialog.svelte';
 
 import { AlertType } from './enum/enum.js';
 
 export { AlertType };
-export { AlertBox, ListPicker, RangeInput, ButtonGroup, Stepper, Stack, ValidatedInput };
+export { AlertBox, ListPicker, RangeInput, ButtonGroup, ModalDialog, Stepper, Stack, ValidatedInput };
