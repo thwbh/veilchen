e08dd26ca1bdf9c8cdb78835939f826694f43050
Stefan Poindl <stefan@tohuwabohu.io>
2026-02-12 13:06:24 +0100

    add missing ResizeObserver and element.animate mocks

--- vitest-setup-client.ts
+++ vitest-setup-client.ts
@@ -3,16 +3,29 @@ import { vi } from 'vitest';
 
 // required for svelte5 + jsdom as jsdom does not support matchMedia
 Object.defineProperty(window, 'matchMedia', {
-	writable: true,
-	enumerable: true,
-	value: vi.fn().mockImplementation((query) => ({
-		matches: false,
-		media: query,
-		onchange: null,
-		addEventListener: vi.fn(),
-		removeEventListener: vi.fn(),
-		dispatchEvent: vi.fn()
-	}))
+  writable: true,
+  enumerable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn()
+  }))
 });
 
 // add more mocks here if you need them
+Object.defineProperty(window, 'ResizeObserver', {
+  value: vi.fn().mockImplementation(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn()
+  }))
+});
+
+Element.prototype.animate = vi.fn().mockImplementation(() => ({
+  cancel: vi.fn(),
+  finished: vi.fn()
+}))
+
